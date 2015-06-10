8aa4b38ab392a5505cfa870831659c6ef27e5097
mrhocuong <mrhocuong@gmail.com>
2015-06-10 08:13:57 +0700

    Create Datie Project

---
+++ DatieProject/Controllers/DatieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatieProject.Controllers
+{
+    public class DatieController : Controller
+    {
+        DatieDBEntities _db = new DatieDBEntities();
+        //
+        // GET: /Datie/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	}
+}
